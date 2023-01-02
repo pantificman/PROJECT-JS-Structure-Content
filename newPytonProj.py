b1860b8bb2f1c4b9931ff3cc69b2ed61bc9a6064
Anton <antonsoldatov1983@gmail.com>
2023-01-02 22:20:34 +0000

    pyTypes

---
+++ newPytonProj.py
@@ -0,0 +1,29 @@
+a, b, c = 10, 20, 30
+print(a)
+print(b)
+print(c)
+
+d, e, f, g = (33, "car", 2.012, "hi")
+print(d)
+print(e)
+print(f)
+print(g)
+
+ages = {"Dave": "41", "Bob": "22", "Mark": "38"}
+for key, value in ages.items():
+    print("F{key} is {value}")
+
+agess = 50
+print(agess.items())
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.items()
+
+car["year"] = 1999
+
+print(x)
