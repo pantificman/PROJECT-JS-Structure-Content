21461985d1c8fc5ad301715708634148123a7613
Anton <antonsoldatov1983@gmail.com>
2023-01-03 23:17:20 +0000

    pythonLoops

--- newPytonProj.py
+++ newPytonProj.py
@@ -91,10 +91,91 @@
 # colors.reverse()
 # print(colors)
 
-players = ["tom", "dick", "mickael", "dan", "adam", "bob", "stive", "anton"]
-print("There are first three players on my team:")
-#for player in players[3:6:2]:
-print(players[3:6])
+# players = ["tom", "dick", "mickael", "dan", "adam", "bob", "stive", "anton"]
+# print("There are first three players on my team:")
+# #for player in players[3:6:2]:
+# print(players[3:6])
 
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
-print(num[1:8:2])
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+# print(num[1:8:2])
+
+# a = 3 + 4
+# print(a)
+
+# b = 5 - 3
+# print(b)
+
+# c = 2*5
+# print(c)
+
+# d = 10/2
+# print(d)
+
+# e = 3**2
+# print(e)
+
+# f = 7
+# f += 5
+# print(f)
+
+# #multiply
+# g = 4
+# g *= 6
+# print(g)
+# #logical operators
+# i = 8
+# print(i < 10 or i > 1)
+
+# #identity operators
+# j = 2
+# k = 6
+# l = j
+# print(j is k)
+# print(l is j)
+
+# z = [5, 3, 7]
+# print(3 in z)
+# print(10 not in z)
+
+# x = 1
+# y = 3
+# if x > y:
+#   print("The x greater than y")
+# elif x == y:
+#   print("equal")
+# else:
+#   print("dghdd")
+
+#   #Without IF statements
+
+# employees = ['Mark', 'Steve', 'Dave', 'Lisa', 'Jordyn']
+
+# if 'Steve' in employees:
+#     print("Steve is an employee")
+# if 'Lisa' in employees:
+#     print("Lisa is an employee")
+# if 'John' in employees:
+#     print("John is an employee")
+
+# print("All done with employee selection")
+
+# for y in range(25,35,5):
+#     print(y)
+
+# for b in range(0, 50, 5):
+#     print(b)
+
+# a = 3
+# b = 6
+# c = 5
+# print(a+b*c)
+# print(a+b/c)
+# print(a+b**c)
+# print(a-b*c)
+
+# while a < 5:
+#   print("ok")
+#   a+=1
+colors = ['blue', 'red', 'pink']
+for x in colors:
+  print(x)
