dec60e7f0a13eb86e23338fa85071e2e58b97df2
Anton <antonsoldatov1983@gmail.com>
2023-01-03 14:47:23 +0000

    pythList

--- newPytonProj.py
+++ newPytonProj.py
@@ -1,62 +1,100 @@
-a, b, c = 10, 20, 30
-print(a)
-print(b)
-print(c)
+# a, b, c = 10, 20, 30
+# print(a)
+# print(b)
+# print(c)
 
-d, e, f, g = (33, "car", 2.012, "hi")
-print(d)
-print(e)
-print(f)
-print(g)
+# d, e, f, g = (33, "car", 2.012, "hi")
+# print(d)
+# print(e)
+# print(f)
+# print(g)
 
-ages = {"Dave": "41", "Bob": "22", "Mark": "38"}
-for key, value in ages.items():
-    print("F{key} is {value}")
+# ages = {"Dave": "41", "Bob": "22", "Mark": "38"}
+# for key, value in ages.items():
+#     print("F{key} is {value}")
 
-agess = 50
-print(agess.items())
+# agess = 50
+# print(agess.items())
 
-car = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
 
-x = car.items()
+# x = car.items()
 
-car["year"] = 1999
+# car["year"] = 1999
 
-print(x)
+# print(x)
 
-float(12)
-print(float(12))
-print(int(12.2))
+# float(12)
+# print(float(12))
+# print(int(12.2))
 
-listExample = ["John", "Robert", "Hillary", "Angus", "David"]
-print(listExample[3] + " is a great employee")
+# listExample = ["John", "Robert", "Hillary", "Angus", "David"]
+# print(listExample[3] + " is a great employee")
 
-listExample = listExample + ["Jim"]
-print(listExample)
+# listExample = listExample + ["Jim"]
+# print(listExample)
 
-listExample2 = [45, 2, 454, 24, 5]
-print(listExample + listExample2)
+# listExample2 = [45, 2, 454, 24, 5]
+# print(listExample + listExample2)
 
-listExample.insert(0, "James")
-print(listExample)
+# listExample.insert(0, "James")
+# print(listExample)
 
-del(listExample[3])
-print(listExample)
+# del(listExample[3])
+# print(listExample)
 
-listExample.remove("Robert")
-print(listExample)
+# listExample.remove("Robert")
+# print(listExample)
 
-for x in listExample:
-  print(x)
+# for x in listExample:
+#   print(x)
 
-  if "David" in listExample:
-    print("This employee is on the list")
+#   if "David" in listExample:
+#     print("This employee is on the list")
 
-listExample3 = ["one", "two", "three", "four"]
+# listExample3 = ["one", "two", "three", "four"]
 
-print(len(listExample3))
+# print(len(listExample3))
 
+# firstList = ["yellow", "brown", "black", "red", "white"]
+# firstList[3] = "orange"
+# print(firstList)
+
+# secondlist = ["birds", "animals", "fishes", "humans", "insects"]
+# del(secondlist[2])
+# print(secondlist)
+
+# thirdlist = []
+# thirdlist.insert(3, "Toyota")
+# thirdlist.insert(4, "BMW")
+# print(thirdlist)
+
+# secondlist = ["birds", "animals", "fishes", "humans", "insects"]
+# secondlist.sort()
+# print(secondlist)
+# secondlist.reverse()
+# #secondlist.sort(reverse=True)
+# print(secondlist)
+
+# colors = ["blue", "red", "green", "yellow"]
+# print("this is the first list")
+# print(colors)
+
+# print("\nThis is a sorted list")
+# colors.sort()
+# print(colors)
+
+# colors.reverse()
+# print(colors)
+
+players = ["tom", "dick", "mickael", "dan", "adam", "bob", "stive", "anton"]
+print("There are first three players on my team:")
+#for player in players[3:6:2]:
+print(players[3:6])
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+print(num[1:8:2])
